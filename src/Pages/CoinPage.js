cdfa9c6a3a50e2fb1c855c70f46741a2a7e06792
itsvikasdwivedi <vickyserver22@gmail.com>
2023-01-05 21:05:13 +0530

    chart data structure

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,8 +1,9 @@
+import { Typography } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { useParams } from 'react-router-dom'
-import CoinInfo from '../Components/CoinInfo';
+// import CoinInfo from '../Components/CoinInfo';
 import { SingleCoin } from '../Config/api';
 import { CryptoState } from '../Context/CryptoContext';
 
@@ -24,17 +25,66 @@ const CoinPage = () => {
 
 
   return (
-    <div>
-      <div>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems : 'center'
+    }} >
+      <div 
+      // style={{
+      //   width : "30%",
+      //   display : "flex",
+      //   alignItems : "center",
+      //   marginTop: "25px",
+      //   borderRight : "2px solid darkgrey"
+      // }}
+      >
         <img src={coin?.image.large}
         alt={coin?.name}
         height = "200"
         style={{
           marginBottom: 20
         }}/>
-        
+
+        <Typography variant='h3' style={{
+          fontWeight: "bold",
+          marginBottom: 20,
+          fontFamily: "Monserrat"
+        }}>
+        {coin?.name}
+        </Typography>
+        <Typography variant='subtitle1'
+        style={{
+          width: "100%",
+          fontFamily: "Monserrat",
+          padding: 25,
+          paddingBottom: 20,
+          paddingTop: 0,
+          textAlign: "center",
+        }}>
+          {coin?.description.en.split(". ")[0]}
+        </Typography>
+      {/* <CoinInfo /> */}
+      <div>
+        <span
+        style={{
+          display: "flex"
+        }}>
+          <Typography variant='h5'>
+            Rank:
+          </Typography>
+          &nbsp; &nbsp;
+          <Typography variant='h5'
+          style={{
+            fontFamily: "Monserrat"
+          }}>
+          </Typography>
+
+        </span>
+      <div/>
+
       </div>
-      <CoinInfo />
+      
     </div>
 
   )

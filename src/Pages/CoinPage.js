2ea5aae1b30e179a2a8d24684c908538412dec54
itsvikasdwivedi <vickyserver22@gmail.com>
2023-01-07 17:02:18 +0530

    zzz

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material';
+import { createTheme, LinearProgress, Typography } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect } from 'react'
 import { useState } from 'react'
@@ -23,70 +23,147 @@ const CoinPage = () => {
     fetchCoin(coin);
   }, [])
 
+  const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+  }
+
+  // const theme = createTheme({
+  //   breakpoints: {
+  //     values: {
+  //       xs: 0,
+  //       sm: 600,
+  //       md: 900,
+  //       lg: 1200,
+  //       xl: 1536,
+  //     },
+  //   },
+  // });
+  
+
+  if (!coin)
+    return (
+      <LinearProgress style={{ backgroundColor: "goldenrod" }} />
+    )
+
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems : 'center'
-    }} >
-      <div 
-      // style={{
-      //   width : "30%",
-      //   display : "flex",
-      //   alignItems : "center",
-      //   marginTop: "25px",
-      //   borderRight : "2px solid darkgrey"
-      // }}
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }} >
+      <div
+      style={{
+        width : "30%",
+        display : "flex",
+        alignItems : "center",
+        marginTop: "25px",
+        borderRight : "2px solid darkgrey"
+      }}
       >
         <img src={coin?.image.large}
-        alt={coin?.name}
-        height = "200"
-        style={{
-          marginBottom: 20
-        }}/>
+          alt={coin?.name}
+          height="200"
+          style={{
+            marginBottom: 20
+          }} />
 
         <Typography variant='h3' style={{
           fontWeight: "bold",
           marginBottom: 20,
           fontFamily: "Monserrat"
         }}>
-        {coin?.name}
+          {coin?.name}
         </Typography>
         <Typography variant='subtitle1'
-        style={{
-          width: "100%",
-          fontFamily: "Monserrat",
-          padding: 25,
-          paddingBottom: 20,
-          paddingTop: 0,
-          textAlign: "center",
-        }}>
+          style={{
+            width: "100%",
+            fontFamily: "Monserrat",
+            padding: 25,
+            paddingBottom: 20,
+            paddingTop: 0,
+            textAlign: "center",
+          }}>
           {coin?.description.en.split(". ")[0]}
         </Typography>
-      {/* <CoinInfo /> */}
-      <div>
-        <span
-        style={{
-          display: "flex"
-        }}>
-          <Typography variant='h5'>
-            Rank:
-          </Typography>
-          &nbsp; &nbsp;
-          <Typography variant='h5'
-          style={{
-            fontFamily: "Monserrat"
-          }}>
-          </Typography>
+        {/* <CoinInfo /> */}
+        <div style={
+          {
+            alignSelf: "start",
+            padding : 25,
+            paddingTop: 20,
+            width: "100%",
+            // [theme.breakpoints.down("md")]:{
+            //   display: "flex",
+            //   justifyContent : "space-around"
+            // },
+            // [theme.breakpoints.down("md")]:{
+            //   flexDirection: "column",
+            //   alignIntems: "center"
+            // },
+            // [theme.breakpoints.down("xs")]:{
+            //   alignItems : "start"
+            // }
+          }
+        }>
+          <span
+            style={{
+              display: "flex"
+            }}>
+            <Typography variant='h5'>
+              Rank:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography variant='h5'
+              style={{
+                fontFamily: "Monserrat"
+              }}>
+              {coin?.market_cap_rank}
+            </Typography>
+          </span>
+          <span
+            style={{
+              display: "flex"
+            }}>
+            <Typography variant='h5'>
+              Current Price:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography variant='h5'
+              style={{
+                fontFamily: "Monserrat"
+              }}>
+              {symbol} {" "}
+              {numberWithCommas(coin?.market_data.current_price[currency.toLowerCase()]
+              )}
+            </Typography>
+          </span>
 
-        </span>
-      <div/>
+          <span
+            style={{
+              display: "flex"
+            }}>
+            <Typography variant='h5'>
+              Market Cap:{" "}
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography variant='h5'
+              style={{
+                fontFamily: "Monserrat"
+              }}>
+              {symbol} {" "}
+              {(coin?.market_data.market_cap[currency.toLowerCase()]
+                .toString()
+                .slice(0, -6))}M
+            </Typography>
+          </span>
+          <div />
+
+        </div>
 
       </div>
-      
     </div>
-
   )
 }
 

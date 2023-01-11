135ceb5d8a9c590b254cdcd6fbd6f9d9e110e844
itsvikasdwivedi <vickyserver22@gmail.com>
2023-01-11 23:31:14 +0530

    Responsiveness Remaining

--- src/Components/SelectButton.js
+++ src/Components/SelectButton.js
@@ -1,27 +1,28 @@
 import React from 'react'
 
-const SelectButton = ({children}) => {
+const SelectButton = ({children,selected,onClick}) => {
+
   return (
     <span
-    // onClick={onClick}
-    // style={{
-    //   border: '1px solid gold',
-    //   borderRadius: '5px',
-    //   padding: '10px',
-    //   paddingLeft: '20px',
-    //   paddingRight: '20px',
-    //   fontFamily: 'Montserrat',
-    //   cursor: 'pointer',
-    //   backgroundColor: selected ? 'gold' : '',
-    //   color: selected ? 'black' : '',
-    //   fontWeight: selected ? 700: 500,
-    //   "&:hover":{
-    //     backgroundColor: 'gold',
-    //     color : 'black',
-    //   },
-    //   widht: '22%',
-    //   margin: 5
-    // }}
+    onClick={onClick}
+    style={{
+      border: '1px solid gold',
+      borderRadius: '5px',
+      padding: '10px',
+      paddingLeft: '20px',
+      paddingRight: '20px',
+      fontFamily: 'Montserrat',
+      cursor: 'pointer',
+      backgroundColor: selected ? 'gold' : '',
+      color: selected ? 'black' : '',
+      fontWeight: selected ? 700: 500,
+      // "&:hover": {
+      //   backgroundColor: 'gold',
+      //   color : 'black',
+      // },
+      widht: '22%',
+      margin: 5
+    }}
     >
       
       {children}</span>
